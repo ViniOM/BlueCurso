506c7ff791fd270a73668ae1fed69ec9ca2a9451
Vinicios Gabriel Oliveira <vini.webdeveloper@gmail.com>
2022-01-22 19:59:59 -0300

    Add files via upload

---
+++ modulo1/semana1/aula04/aula04.js
@@ -0,0 +1,31 @@
+
+
+
+
+// // const compara = 5 > 3;
+// // console.log(compara);
+// // console.log(typeof(compara));
+// let var1 = 10;
+
+// console.log(var1);
+// console.log(typeof(var1));
+
+// if(var1 == 10){
+//     console.log('É true!!!')
+// }
+
+const prompt = require('prompt-sync')();
+
+
+let local = prompt("Onde o Manuel esta");
+
+if(local == 'rio' ){
+    console.log('vai ver o vasco');
+}else if(local == 'bahia'){
+    console.log('Vai descansar')
+}
+else {
+    console.log('Vai passear com a namorada');
+}
+
+
